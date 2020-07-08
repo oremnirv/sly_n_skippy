8c00061ad34db7abc16ae63456ca3d8ee72a32f7
Omer Nivron <omernivr12+git@gmail.com>
2020-07-08 21:26:15 +0200

    sth doesnt work in data combination function -- the times are not being assigned

--- helpers/helper.py
+++ helpers/helper.py
@@ -36,19 +36,30 @@ def data_combination(foxes_ode, rabbits_ode, foxes_prob, rabbits_prob, ode_t, pr
 
     n_columns = seq_max_len_w_o_padding(
         foxes_ode) + seq_max_len_w_o_padding(foxes_prob)
+
     combined_array = np.zeros((foxes_ode.shape[0] * 5, n_columns))
+
     for idx in range(foxes_ode.shape[0]):
+
         ode_m = np.where(foxes_ode[idx, :] != 0)[0][-1] + 1
         prob_m = np.where(foxes_prob[idx, :] != 0)[0][-1] + 1
+
         m = ode_m + prob_m
+
         combined_array[idx, :m] = np.concatenate(
             (foxes_ode[idx, :ode_m], foxes_prob[idx, :prob_m]))
+
         combined_array[idx + 1, :m] = np.concatenate(
             (rabbits_ode[idx, :ode_m], rabbits_prob[idx, :prob_m]))
-        combined_array[idx + 2,
+
+
+        combined_array[(idx + 2),
                        :m] = np.concatenate((ode_t[idx, :ode_m], prob_t[idx, :prob_m]))
-        combined_array[idx + 3,
+
+        combined_array[(idx + 3),
                        :m] = np.concatenate((np.ones(ode_m), np.ones(prob_m) * 2))
-        combined_array[idx + 4,
+
+        combined_array[(idx + 4),
                        :m] = np.concatenate((np.ones(ode_m) * 3, np.ones(prob_m) * 4))
+
     return combined_array
